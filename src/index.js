fcc57bb4d742b392ed1547ddc47395882690077d
wenhai03 <810068733@qq.com>
2021-01-25 10:24:23 +0800

    feat：解决useState状态共用

--- src/index.js
+++ src/index.js
@@ -16,12 +16,31 @@ function useState (initialState) {
   
   return [memoizedStates[index++], setState]
 }
+/*
+* 在函数主体中，不能具有副作用的逻辑，订阅，定时器修改DOM
+* useEffect 给函数组件添加了操作副作用的
+* 类组件 didmount didupdate willunmount
+*
+* */
+let lastDependencies
 
+function useEffect (callback, dependencies) {
+  if (!dependencies) return callback()
+  let changed = lastDependencies ? !dependencies.every((item, index) => item === lastDependencies[index]) : true
+  if (changed) {
+    callback()
+    lastDependencies = dependencies
+  }
+}
 
 function Counter () {
   const [name, setName] = useState('计数器')
   const [number, setNumber] = useState(1)
   
+  useEffect(() => {
+    // 解决number每次都打印？？
+    console.log('number -> ', number)
+  }, [number])
   
   return (
     <>
